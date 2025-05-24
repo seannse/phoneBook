99413235a60c06f10052757b359c302fe79a7a8d
Anna <seannse@gmail.com>
2025-05-24 14:17:57 +0300

    fix exp

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
-import { ModalCloseBtn } from '..';
+import { ModalCloseBtn } from '../../components';
 import { useWindowSize } from '../../hooks/useWindowSize';
 
 import { ModalBackdrop } from '../../components';
